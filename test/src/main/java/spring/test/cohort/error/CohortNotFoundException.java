9f1aaf453eeb768fea9aa3761c37dfa20d736f60
dungle0406 <94211332+dungle0406@users.noreply.github.com>
2024-01-05 15:39:53 +0700

    )Auth properties

--- test/src/main/java/spring/test/cohort/error/CohortNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package spring.test.cohort.error;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "no cohort found in the database with the provided id")
-public class CohortNotFoundException extends RuntimeException{
-}
--- test/src/main/java/spring/test/mentor/error/LackOfInformation.java
+++
@@ -1,9 +0,0 @@
-package spring.test.mentor.error;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
-
-public class LackOfInformation extends Exception {
-}
