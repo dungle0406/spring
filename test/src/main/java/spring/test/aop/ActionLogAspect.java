c78fcead0531fb0ff7fd027e64d21c7bd7980789
dungle0406 <94211332+dungle0406@users.noreply.github.com>
2024-01-09 01:43:26 +0700

    seo

--- test/src/main/java/spring/test/aop/ActionLogAspect.java
+++ test/src/main/java/spring/test/aop/ActionLogAspect.java
@@ -6,11 +6,13 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import spring.test.mentor.error.InvalidRatingBadRequest;
-import spring.test.mentor.error.LackOfInformation;
 
 import java.time.Instant;
 
@@ -24,33 +26,15 @@ public class ActionLogAspect {
     public ActionLogAspect(ActionLogService actionLogService) {
         this.actionLogService = actionLogService;
     }
-
-    @Pointcut(value = "execution(* spring.test.mentor.service.MentorService.*(..))")
-    public void printLogs() {
-    }
-
-    @Before("execution(* spring.test.mentor.service.MentorService.*(..))")
-    public void printLogsStatementsBefore() {
-        System.out.println("If there is no @Around, @Before will be called first!");
-    }
-
-    @After("execution(* spring.test.mentor.service.MentorService.*(..))")
-    public void printLogsStatementsAfter() {
-        System.out.println("If there is no @Around, @After will be called after @Before(if available!)!");
-    }
-
-    @AfterReturning(value = "execution(* spring.test.mentor.service.MentorService.*(..))", returning = "mentors")
-    public void logAfterRunningDisplay(JoinPoint joinpoint, Object mentors) {
-        System.out.println("After Returning method:" + joinpoint.getSignature());
-    }
-
-    @AfterThrowing(value = "execution(* spring.test.mentor.service.MentorService.*(..))", throwing = "ex")
+    @AfterThrowing(value = "execution(* spring.test.mentor..*.*(..))", throwing = "ex")
     public Object handleExceptions(Throwable ex) {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
 
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
 
-        if(!(ex instanceof UnsupportedOperationException)) {
+        if (ex instanceof HttpClientErrorException.MethodNotAllowed || ex instanceof HttpRequestMethodNotSupportedException) {
+            httpStatus = HttpStatus.METHOD_NOT_ALLOWED;
+        } else if (ex instanceof HttpMessageNotReadableException || ex instanceof MissingServletRequestParameterException) {
             httpStatus = HttpStatus.BAD_REQUEST;
         }
 
